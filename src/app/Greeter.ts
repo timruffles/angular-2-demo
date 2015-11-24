f524880fbc89d245c87e47540495992f50751e7d
Tim Ruffles <timruffles@gmail.com>
2015-11-24 07:17:33 +0000

    add in greeter

---
+++ src/app/Greeter.ts
@@ -0,0 +1,8 @@
+export { Greeter };
+
+class Greeter {
+  message(name: string) {
+    return `Hello ${name}, Angular 2 is easy`;
+  }
+}
+
--- src/app/HelloComponent.ts
+++ src/app/HelloComponent.ts
@@ -1,17 +1,16 @@
 import { Component } from 'angular2/angular2';
+import { Greeter } from './Greeter';
 
 export { HelloComponent };
 
 @Component({
   selector: 'hello-angular',
+  providers: [Greeter],
   template: '<h1>{{ message }}</h1>',
 })
 class HelloComponent {
-  message: string;
-
-  constructor() { 
-    this.message = "Hi there";
+  constructor(greeter: Greeter) { 
+    this.message = greeter.message("js-monthly");
   }
 }
-
 
